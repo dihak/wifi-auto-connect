71defe100985ec526ecf1f951508d5a6aca4c8b4
Dihak <dihak@digitalkode.com>
2021-08-25 08:25:26 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# WIFI AUTO CONNECT
+
+Auto re-connect to a wifi network after a disconnection.
+
+## USAGE
+
+```
+python3 wifi_auto_connect.py <ping-desitination> <essid>
+```
---
+++ autoconnect.py
@@ -0,0 +1,38 @@
+import platform    # For getting the operating system name
+import subprocess  # For executing a shell command
+import sys
+import time
+
+if len(sys.argv) != 3:
+    print("Usage: python3 autoconnect.py <ip_address> <essid>")
+    sys.exit(1)
+
+ip_address = str(sys.argv[1])
+essid = str(sys.argv[2])
+
+def ping(host):
+    status,result = subprocess.getstatusoutput("ping -c1 -w2 " + host)
+    ms = result.split("time=")[1].split(" ")[0] # get the time in ms
+
+    return [status == 0, ms + "ms"]
+
+def connectToWifi():
+    """
+    Connects to the wifi network with the given essid and password.
+    Returns True if connection is successful, False otherwise.
+    """
+
+    command = ['nmcli', 'dev', 'wifi', 'connect', essid]
+
+    return subprocess.call(command) == 0
+
+while True:
+    [connected, ms] = ping(ip_address)
+    if connected:
+        print("Connected to " + ip_address, ms)
+    else:
+        print("Could not connect to " + ip_address)
+        print("Re-connect wifi to " + essid)
+        if connectToWifi():
+            print("Connected to " + essid)
+    time.sleep(5)
